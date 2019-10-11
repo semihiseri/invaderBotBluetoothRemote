955a14de1850a4812b61f8191930773a071be34d
semih <semih.iseri@gmail.com>
2019-10-11 23:50:10 +0300

    basic client -transmit only

--- client.py
+++ client.py
@@ -1,11 +1,22 @@
 from bluetooth import *
 
+devAddress = "" # fill the device address here. 
+
+if devAddress == "":
+    print "edit the file and configure the device address!"
+    quit()
+
 # Create the client socket
 client_socket=BluetoothSocket( RFCOMM )
 
-client_socket.connect(("00:12:D2:5A:BD:E4", 3))
+client_socket.connect((devAddress, 3))
 
-client_socket.send("Hello World")
+while True:
+    try:
+        data = str(raw_input())
+        client_socket.send(data)
+    except:
+        break
 
 print "Finished"
 
